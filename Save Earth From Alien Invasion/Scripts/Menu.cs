44cb7dffb2f8fef8e269ea376a13193af417eeb6
Igor Oliveira <igore.m.oliveira@gmail.com>
2022-10-30 13:19:49 -0300

    Create Menu.cs

---
+++ Save Earth From Alien Invasion/Scripts/Menu.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    // armazena o panel de creditos 
+    [SerializeField]
+    GameObject _painelDeCreditos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void IniciarJogo()
+    {
+        // carrega primeira fase
+
+        SceneManager.LoadScene("Fase 1");
+    }
+
+    public void Creditos()
+    {
+        // habilita o painel de creditos
+        _painelDeCreditos.SetActive(true);
+    }
+
+    public void RetornarAoMenu()
+    {
+        // Desabilitar o painel de creditos
+        _painelDeCreditos.SetActive(false);
+    }
+
+    public void SairDoJogo()
+    {
+        Application.Quit();
+
+        Debug.Log("Saiu do jogo");
+    }
+}
