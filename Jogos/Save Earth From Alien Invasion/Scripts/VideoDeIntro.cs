09162c33ce901c523d9188792ceea21d6ce97724
Igor Oliveira <igore.m.oliveira@gmail.com>
2022-11-04 20:07:34 -0300

    Create VideoDeIntro.cs

---
+++ Jogos/Save Earth From Alien Invasion/Scripts/VideoDeIntro.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VideoDeIntro : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("CarregaMenu", 10);
+    }
+
+
+    private void CarregaMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
