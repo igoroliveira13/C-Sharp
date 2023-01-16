947bb3274b5c5ca9a490959cb3aa41cc316107eb
Igor Oliveira <igore.m.oliveira@gmail.com>
2023-01-16 18:53:43 -0300

    Create Imagem.cs

---
+++ Jogos/Its Raining Saws, Stay Alive Mobile/Cena Jogo/Imagem.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Imagem : MonoBehaviour
+{
+    // Este script pega o tamanho da tela e faz a imagem se adequar a tela
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpriteRenderer sprite = GetComponentInChildren<SpriteRenderer>();
+        Vector3 escala = GetComponentInChildren<Transform>().transform.localScale;
+
+        float largura = sprite.bounds.size.x;
+        float altura = sprite.bounds.size.y;
+
+        float alturaCamera = Camera.main.orthographicSize * 2.0f;
+        float larguraDaTela = alturaCamera / Screen.height * Screen.width;
+
+        escala.x = larguraDaTela / largura;
+        escala.y = alturaCamera / altura;
+
+        transform.localScale = escala;
+    }
+}
